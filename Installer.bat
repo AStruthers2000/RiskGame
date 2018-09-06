d5c48361ae4b0ea0c0b9878e0b8cf8cd7d250c96
Andrew Struthers <struthers919@gmail.com>
2018-09-06 14:22:03 -0700

    Got rid of installer

--- Installer.bat
+++ Installer.bat
@@ -1,5 +1,6 @@
-@echo off
-cd ..
-echo git clone "https://github.com/Computer-Nerd123/RiskGame.git" >> Installer.bat
-start Installer.bat
-pause
+rem @echo off
+rem cd ..
+rem echo rmdir /s /q RiskGame && start Installer.bat >> remover.bat
+rem echo git clone "https://github.com/Computer-Nerd123/RiskGame.git" >> Installer.bat
+rem start remover.bat
+rem pause
