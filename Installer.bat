f4b555ee6b8bd2ceb90ffc7746ce9875f6d5744c
Andrew Struthers <struthers919@gmail.com>
2018-09-06 14:15:47 -0700

    Installer fixed again (probably not)

--- Installer.bat
+++ Installer.bat
@@ -1,5 +1,5 @@
 @echo off
 cd ..
-echo rmdir /s /q RiskGame && git clone "https://github.com/Computer-Nerd123/RiskGame.git" >> Installer.bat
+echo git clone "https://github.com/Computer-Nerd123/RiskGame.git" >> Installer.bat
 start Installer.bat
 pause
